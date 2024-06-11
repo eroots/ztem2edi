6e71cbad2333a959829beff84de23b4841ab03dc
eroots <eroots087@gmail.com>
2024-06-11 16:18:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(name='ztem2edi',
       scripts=[],
       entry_points={'console_scripts': ['ztem2edi = ztem2edi.ztem_to_edi:main']},
       install_requires=['numpy==1.20.3',
-                        'pandas>=2.0, <3',
+                        'pandas==2.0',
                         'geosoft'],
       include_package_data=False)
